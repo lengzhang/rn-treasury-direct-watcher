d2944a735d33461d9ab2c4aa9406a94b3ab25dfe
lengzhang <lengzhang@live.com>
2024-04-20 13:03:10 -0700

    [config] update config for eas

--- app.config.js
+++ app.config.js
@@ -1,19 +1,23 @@
 module.exports = {
     expo: {
-        name: 'rn-treasury-direct-watcher',
-        slug: 'rn-treasury-direct-watcher',
+        name: `TD Watcher${process.env.APP_ENV === 'production' ? '' : ` (${process.env.APP_ENV})`}`,
+        description:
+            'This application provides visibility to client for Treasury Direct securities.',
+        slug: 'treasury-direct-watcher',
         version: '0.0.0',
+        githubUrl: 'https://github.com/lengzhang/rn-treasury-direct-watcher',
         orientation: 'portrait',
         icon: './assets/icon.png',
-        userInterfaceStyle: 'light',
+        userInterfaceStyle: 'automatic',
         splash: {
             image: './assets/splash.png',
             resizeMode: 'contain',
-            backgroundColor: '#ffffff'
+            backgroundColor: '#00000'
         },
         assetBundlePatterns: ['**/*'],
         ios: {
-            supportsTablet: true
+            supportsTablet: true,
+            bundleIdentifier: 'com.lengzhang.treasury-direct-watcher'
         },
         android: {
             adaptiveIcon: {
@@ -23,6 +27,17 @@ module.exports = {
         },
         web: {
             favicon: './assets/favicon.png'
+        },
+        extra: {
+            eas: {
+                projectId: 'eff24fc7-5a1f-435d-88a1-5722852d7131'
+            }
+        },
+        updates: {
+            url: 'https://u.expo.dev/eff24fc7-5a1f-435d-88a1-5722852d7131'
+        },
+        runtimeVersion: {
+            policy: 'appVersion'
         }
     }
 }
